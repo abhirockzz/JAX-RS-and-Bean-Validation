ccab74fb71e4dfe5b6745959be27db07f76a68fc
abhi <abhi@192.168.1.100>
2016-01-22 11:38:18 +0530

    JAX-RS resource is now an EJB (Stateless) as well

--- src/main/java/abhirockzz/wordpress/com/payara611/UsersResource.java
+++ src/main/java/abhirockzz/wordpress/com/payara611/UsersResource.java
@@ -1,6 +1,7 @@
 
 package abhirockzz.wordpress.com.payara611;
 
+import javax.ejb.Stateless;
 import javax.validation.Valid;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -9,7 +10,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
-
+@Stateless
 @Path("users")
 public class UsersResource {
     
